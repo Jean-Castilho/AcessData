c56c50b1eea9537c0f65cc8a0a87404db361296a
Jean-Castilho <jeancastilhoSz@proton.me>
2025-04-17 20:31:42 -0300

    refactor: improve product controller methods and enhance error handling in routes

--- src/controllers/ProductsControllers.js
+++ src/controllers/ProductsControllers.js
@@ -11,65 +11,30 @@ export default class ProductsControllers {
     this.products = this.database.collection("products");
   }
 
-  async getProductById(req, res) {
-    try {
-      const { id } = req.params;
-      const product = await this.products.findOne({ _id: id });
-      if (!product) {
-        return res.status(404).json({ message: "Producto naão encontrado" });
-      }
-      return product;
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
+  async getProductById(id) {
+    const product = await this.products.findOne({ _id: id });
+    if (!product) {
+      return res.status(404).json({ message: "Producto naão encontrado" });
     }
+    return product;
   }
 
   async getProducts(req, res) {
-    try {
-      const products = await this.products.find().toArray();
-      return products;
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
+    return (products = await this.products.find().toArray());
   }
 
   async createProduct(productData) {
-
-    const result = await this.products.insertOne(productData);
-
-    return result;
+    return (result = await this.products.insertOne(productData));
   }
 
-  async updateProduct(req, res) {
-    try {
-      const { id } = req.params;
-      const { nome, preco, descricao, imagem } = req.body;
-      const updatedProduct = { nome, preco, descricao, imagem };
-      const result = await this.products.updateOne(
-        { _id: id },
-        { $set: updatedProduct }
-      );
-      if (result.matchedCount === 0) {
-        return res.status(404).json({ message: "Produto não encontrado" });
-      }
-      return res
-        .status(200)
-        .json({ message: "Produto atualizado com sucesso" });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
+  async updateProduct(id, updatedProduct) {
+    return (result = await this.products.updateOne(
+      { _id: id },
+      { $set: updatedProduct }
+    ));
   }
 
-  async deleteProduct(req, res) {
-    try {
-      const { id } = req.params;
-      const result = await this.products.deleteOne({ _id: id });
-      if (result.deletedCount === 0) {
-        return res.status(404).json({ message: "Produto não encontrado" });
-      }
-      return res.status(200).json({ message: "Produto deletado com sucesso" });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
+  async deleteProduct(id) {
+    return (result = await this.products.deleteOne({ _id: id }));
   }
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -9,7 +9,6 @@ const router = express.Router();
 
 const productsController = new ProductsControllers();
 
-
 router.post("/upload", upload.single("file"), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "Nenhum arquivo foi enviado" });
@@ -52,14 +51,64 @@ router.post("/upload", upload.single("file"), async (req, res) => {
   });
 });
 
-router.get("/getProductById/:id", getProductById);
+router.get("/getProductById/:id", async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json({
+        message: "Produto encontrado",
+        product: await productsController.getProductById(req.params.id),
+      });
+  } catch (error) {
+    return res.status(500).json({ message: "Erro ao encontrar o produto" });
+  }
+});
 
-router.get("/getProducts", getProducts);
+router.get("/getProducts", async (req,res) => {
+  try{
+    const products = await productsController.getProducts();
+    return res.status(200).json(products);
+  }catch(error) {
+    return res.status(500).json({ message: "Erro ao obter produtos" });
+  }
+});
 
-router.post("/createProduct", createProduct);
 
-router.put("/updateProduct/:id", updateProduct);
 
-router.delete("/deleteProduct/:id", deleteProduct);
+router.put("/updateProduct/:id", async (req, res) => {
+
+  try {
+    const { id } = req.params;
+    const { nome, preco, descricao, imagem } = req.body;
+    const updatedProduct = { nome, preco, descricao, imagem };
+    const result = await productsController.updateProduct(id, updatedProduct);
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    return res.status(200).json({ message: "Produto atualizado com sucesso" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+});
+
+router.delete("/deleteProduct/:id", async (req,res) => {
+  try {
+    
+    const { id } = req.params;
+    const result = await productsController.deleteProduct(id);
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    return res.status(200).json({ message: "Produto deletado com sucesso" });
+
+  } catch (error) {
+    
+  }
+});
 
 export default router;
